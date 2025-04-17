3f8ffb95b46f6a074ab3fb1ebcf9c51d3a3945f5
onuralp-t <onuralptamer41@gmail.com>
2025-04-17 05:56:54 +0300

    update test.bat

---
+++ bat_scripts_to_test/create_create_test_colon.bat
@@ -0,0 +1,35 @@
+@echo off
+> generated_create_test_colon.bat (
+echo @echo off
+echo ^> generated_test_colon.bat ^(
+echo echo @echo off
+echo echo.        
+echo echo ^^^:^^^: .cpp .exe NAMES
+echo echo set SRC_NAMES^^^=main.cpp
+echo echo set EXE_NAME^^^=main.exe
+echo echo.
+echo echo set exe^^^=-o %%%%EXE_NAME%%%%
+echo echo set asm^^^=-S -masm^^^=intel
+echo echo.
+echo echo ^^^:^^^: Flags to put before source code, specify executable or assembly
+echo echo set before_flags^^^=-static -std^^^=c++23 -O2 %%%%exe%%%%
+echo echo.
+echo echo ^^^:^^^: Include directories
+echo echo set include1^^^=C^^^:\dev\libraries\raylib\src
+echo echo set include_dir^^^=-I"%%%%include1%%%%"
+echo echo.
+echo echo ^^^:^^^: Library directories
+echo echo set lib1^^^=C^^^:\dev\libraries\raylib\src
+echo echo set lib_dir^^^=-L"%%%%lib1%%%%"
+echo echo.
+echo echo ^^^:^^^: Libraries to link
+echo echo set linking^^^=-l^^^:libraylib.a -lopengl32 -lgdi32 -lwinmm
+echo echo.
+echo echo @echo on
+echo echo g++ %%%%before_flags%%%% %%%%SRC_NAMES%%%% %%%%include_dir%%%% %%%%lib_dir%%%% %%%%linking%%%%
+echo echo @echo off
+echo echo.
+echo echo @echo off
+echo echo %%%%EXE_NAME%%%% ^^^&^^^& del %%%%EXE_NAME%%%%
+echo ^)
+)
--- test.bat
+++ test.bat
@@ -1,3 +1,4 @@
+@echo off
 cd bat_scripts_to_test
 
 ..\batify.exe .\test_colon.bat
@@ -5,7 +6,9 @@ cd bat_scripts_to_test
 ..\batify.exe .\create_create_test_colon.bat
 ..\batify.exe .\create_create_create_test_colon.bat
 
-..\batify.exe .\test_rem.bat
-..\batify.exe .\create_test_rem.bat
-..\batify.exe .\create_create_test_rem.bat
-..\batify.exe .\create_create_create_test_rem.bat
+:: ..\batify.exe .\test_rem.bat
+:: ..\batify.exe .\create_test_rem.bat
+:: ..\batify.exe .\create_create_test_rem.bat
+:: ..\batify.exe .\create_create_create_test_rem.bat
+
+echo files outputted to .\bat_scripts_to_test
